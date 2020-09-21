03f2dcd1dc08ab8845bf5882458c78530973d0d5
xXDaniel123 <daniel91dante@gmail.com>
2020-09-21 17:15:11 +0300

    added menu functionality

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Group } from './Group'
 import { NewItem } from './NewItem'
-import { AddNewGroup } from './BoardHeader/GroupCmps/AddNewGroup'
+import { AddNewGroup } from './GroupCmps/AddNewGroup'
 // import { render } from '@testing-library/react'
 
 import { Droppable } from 'react-beautiful-dnd'
