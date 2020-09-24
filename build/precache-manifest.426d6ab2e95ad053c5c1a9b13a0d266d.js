5db5227ebabfb68693bc938927c102dae799a1c9
EladBecker <becker.elad@gmail.com>
2020-09-24 12:18:16 +0300

    after heroku

---
+++ build/precache-manifest.426d6ab2e95ad053c5c1a9b13a0d266d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5f2fe1411cdcb005ce5721a58557c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490d8a5612d65e144ae8",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ce858ef084aaf1c7ddea",
+    "url": "/static/css/main.eb556d09.chunk.css"
+  },
+  {
+    "revision": "490d8a5612d65e144ae8",
+    "url": "/static/js/2.24182ede.chunk.js"
+  },
+  {
+    "revision": "06da8db6d0cc760b636d3fcfcef95394",
+    "url": "/static/js/2.24182ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce858ef084aaf1c7ddea",
+    "url": "/static/js/main.ea2fb978.chunk.js"
+  },
+  {
+    "revision": "3345a06e81732d951e6e",
+    "url": "/static/js/runtime-main.57446094.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "6b605a0ce13d45140618cd05c18ec7ec",
+    "url": "/static/media/bg-1.81fd7471.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a875aa457cdae1389fa7a1f69dc7646e",
+    "url": "/static/media/snorelax.d7380d15.png"
+  }
+]);
