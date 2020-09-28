1498ce87f3ebbd2b27916997292c44b131f632cc
EladBecker <becker.elad@gmail.com>
2020-09-28 19:26:24 +0300

    kippur

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 import yaron from '../assets/imgs/yaron.jpeg';
-import elad from '../assets/imgs/elad.jpeg';
+import elad from '../assets/imgs/elad2.jpeg';
 const logo = require('../assets/imgs/opus.png')
 
 
@@ -48,27 +51,46 @@ export function Main(props) {
             <section className="about">
                 <div className="about-intro">
                     <h2>Meet the Team</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestias possimus amet vel facere, quod officiis consequatur commodi quam repudiandae laboriosam, omnis optio quaerat debitis accusantium quae deleniti, esse sed!</p>
+                    <p>We are 3 of  &copy;MisterBit Coding Academy alumni that came together and assmebled this project for the Full-Stack Developer course's final sprint</p>
                 </div>
                 <div className="about-cards-container">
                     <div className="about-card">
                         <div className="photo">
-                            <img src={yaron} alt="Yaron" />
+                            <img src={yaron} alt="Yaron Lipshitz" />
                         </div>
                         <h3>Yaron Lipshitz</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vitae molestias omnis sapiente ex ducimus maxime nam ipsum at. Impedit neque quae beatae corrupti doloremque adipisci ut quis! Eaque, assumenda.</p>
+                        <div className="links-container">
+                            <a href="https://www.facebook.com/elad.becker"><FacebookIcon className="facebook-icon" /></a>
+                            <a href="https://www.linkedin.com/in/elad-becker-13b4921a6/"><LinkedInIcon className="linkedin-icon" /></a>
+                            <a href="mailto:Becker.Elad@gmail.com"><EmailIcon className="mail-icon" /></a>
+                        </div>
                     </div>
                     <div className="about-card">
                         <div className="photo">
-                            <img src={elad} alt="Elad" />
+                            <img src={elad} alt="Elad Becker" />
                         </div>
                         <h3>Elad Becker</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vitae molestias omnis sapiente ex ducimus maxime nam ipsum at. Impedit neque quae beatae corrupti doloremque adipisci ut quis! Eaque, assumenda.</p>
+                        <p>
+                            Full-Stack Developer<br />
+                            Former IDF Intelligence Officer (Maj.)<br />
+                            Whiskey Enthisiast
+                        </p>
+                        <div className="links-container">
+                            <a href="https://www.facebook.com/elad.becker"><FacebookIcon className="facebook-icon" /></a>
+                            <a href="https://www.linkedin.com/in/elad-becker-13b4921a6/"><LinkedInIcon className="linkedin-icon" /></a>
+                            <a href="mailto:Becker.Elad@gmail.com"><EmailIcon className="mail-icon" /></a>
+                        </div>
                     </div>
                     <div className="about-card">
                         <div className="photo"></div>
                         <h3>Daniel Dante</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vitae molestias omnis sapiente ex ducimus maxime nam ipsum at. Impedit neque quae beatae corrupti doloremque adipisci ut quis! Eaque, assumenda.</p>
+                        <div className="links-container">
+                            <a href="https://www.facebook.com/elad.becker"><FacebookIcon className="facebook-icon" /></a>
+                            <a href="https://www.linkedin.com/in/elad-becker-13b4921a6/"><LinkedInIcon className="linkedin-icon" /></a>
+                            <a href="mailto:Becker.Elad@gmail.com"><EmailIcon className="mail-icon" /></a>
+                        </div>
                     </div>
                 </div>
             </section>
