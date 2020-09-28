50430db32f169891d418a88374a4f0ff141daec0
EladBecker <becker.elad@gmail.com>
2020-09-28 23:41:26 +0300

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 import yaron from '../assets/imgs/yaron.jpeg';
-import elad from '../assets/imgs/elad.jpeg';
+import elad from '../assets/imgs/elad2.jpeg';
 const logo = require('../assets/imgs/opus.png')
 
 
@@ -11,8 +14,10 @@ export function Home(props) {
                 <div className="bg">
                     <div className="bg-overlay">
                         <div className="header">
-                            <img src={logo} alt="logo" />
-                            <h1>Welcome To Opus</h1>
+                            <div className="welcome">
+                                <h1>Welcome To</h1>
+                                <span><img src={logo} alt="logo" /><h1>!</h1></span>
+                            </div>
                         </div>
                         <p>Opus is a beautiful Kanban style project managment software prefect for keeping track of teams' progress</p>
                         <div onClick={() => props.history.push('/board')} className="main-screen-cta">See a Demo</div>
@@ -50,7 +55,8 @@ export function Home(props) {
             <section className="about">
                 <div className="about-intro">
                     <h2>Meet the Team</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestias possimus amet vel facere, quod officiis consequatur commodi quam repudiandae laboriosam, omnis optio quaerat debitis accusantium quae deleniti, esse sed!</p>
+                    <p>We are 3 of  &copy;MisterBit Coding Academy alumni that came together and assmebled <b>OPUS</b> as our Full-Stack Developer course's final hand-in.</p>
+
                 </div>
                 <div className="about-cards-container">
                     <div className="about-card">
@@ -59,18 +65,37 @@ export function Home(props) {
                         </div>
                         <h3>Yaron Lipshitz</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vitae molestias omnis sapiente ex ducimus maxime nam ipsum at. Impedit neque quae beatae corrupti doloremque adipisci ut quis! Eaque, assumenda.</p>
+                        <div className="links-container">
+                            <a href="https://www.facebook.com/elad.becker"><FacebookIcon className="facebook-icon" /></a>
+                            <a href="https://www.linkedin.com/in/elad-becker-13b4921a6/"><LinkedInIcon className="linkedin-icon" /></a>
+                            <a href="mailto:Becker.Elad@gmail.com"><EmailIcon className="mail-icon" /></a>
+                        </div>
                     </div>
                     <div className="about-card">
                         <div className="photo">
                             <img src={elad} alt="Elad" />
                         </div>
                         <h3>Elad Becker</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vitae molestias omnis sapiente ex ducimus maxime nam ipsum at. Impedit neque quae beatae corrupti doloremque adipisci ut quis! Eaque, assumenda.</p>
+                        <p>
+                            Full-Stack Developer<br />
+                            Former IDF Intelligence Officer (Maj.)<br />
+                            Whiskey Enthisiast
+                        </p>
+                        <div className="links-container">
+                            <a href="https://www.facebook.com/elad.becker"><FacebookIcon className="facebook-icon" /></a>
+                            <a href="https://www.linkedin.com/in/elad-becker-13b4921a6/"><LinkedInIcon className="linkedin-icon" /></a>
+                            <a href="mailto:Becker.Elad@gmail.com"><EmailIcon className="mail-icon" /></a>
+                        </div>
                     </div>
                     <div className="about-card">
                         <div className="photo"></div>
                         <h3>Daniel Dante</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vitae molestias omnis sapiente ex ducimus maxime nam ipsum at. Impedit neque quae beatae corrupti doloremque adipisci ut quis! Eaque, assumenda.</p>
+                        <div className="links-container">
+                            <a href="https://www.facebook.com/elad.becker"><FacebookIcon className="facebook-icon" /></a>
+                            <a href="https://www.linkedin.com/in/elad-becker-13b4921a6/"><LinkedInIcon className="linkedin-icon" /></a>
+                            <a href="mailto:Becker.Elad@gmail.com"><EmailIcon className="mail-icon" /></a>
+                        </div>
                     </div>
                 </div>
             </section>
